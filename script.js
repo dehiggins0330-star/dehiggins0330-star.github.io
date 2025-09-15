e8a40c3fbcb30333c8b95fe5e37ee213c0f20bce
dehig <de.higgins0330@gmail.com>
2025-09-14 20:20:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Open/Close Windows
+const icons = document.querySelectorAll('.icon');
+const windows = document.querySelectorAll('.window');
+const closeButtons = document.querySelectorAll('.close-btn');
+
+icons.forEach(icon => {
+  icon.addEventListener('click', () => {
+    const win = document.getElementById(icon.getAttribute('data-window'));
+    win.style.display = 'block';
+    win.style.top = '50px';
+    win.style.left = '50px';
+  });
+});
+
+closeButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.parentElement.parentElement.style.display = 'none';
+  });
+});
+
+// Fun Time Slots
 const slots = document.querySelectorAll('.slot');
 const info = document.getElementById('slot-info');
 
